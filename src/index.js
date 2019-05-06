1a330a6fe3e66f9a3cb13680a97afe6d6fdc5601
= <=>
2019-05-05 22:30:14 -0300

    First commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// import Primeiro from './componentes/Primeiro'
+// import BomDia from './componentes/BomDia'
+// import Multi, { BoaNoite} from './componentes/Multiplus'
+// import Saudacao from './componentes/Saudacao'
+import Pai from './componentes/Pai'
+import Filho from './componentes/FIlho'
+ReactDOM.render(
+    <div>
+        <Pai nome="Paulo" sobrenome="Silva" >
+            <Filho nome='Pedro' sobrenome='Silva' />
+            <Filho nome='Carla' sobrenome='Silva' />
+            <Filho nome='Paulo' sobrenome='Silva' />
+        </Pai>
+       
+    </div>,
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+document.getElementById('root'))
